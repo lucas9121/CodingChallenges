dae0b8e0ba9ff22fe5832aab24efaf0a810260eb
Lucas Carlos <lucas2carlos@gmail.com>
2026-05-16 00:26:52 -0400

    improving code

--- set1.js
+++ set1.js
@@ -11,7 +11,7 @@ function countPositives(arr){
     return count;
 }
 
-countPositives([-2, 0, 3, 5, -1])
+console.log("count positives: ", countPositives([-2, 0, 3, 5, -1]))
 
 
 // 2. Beginner — Find First Even
@@ -24,20 +24,17 @@ function findFirstEven(nums){
     return null
 }
 
-findFirstEven([1, 7, 9, 12, 15]) 
+console.log("find first even: ", findFirstEven([1, 7, 9, 12, 15]) )
 
 
 // 3. Intermediate — Filter Greater Than Target
 // Write a function greaterThan(nums, target) that returns a new array containing only numbers greater than target.
-function greaterThan(arr, target){
+function greaterThan(nums, target){
     const answerArr = [];
-    for(let num of arr){
-        if(num > target) answerArr.push(num)
-    }
-    return answerArr
+    return nums.filter((num) => num > target);
 };
 
-greaterThan([3, 8, 1, 10, 5], 5);
+console.log("greater than: ", greaterThan([3, 8, 1, 10, 5], 5));
 
 
 // 4. Intermediate — Find All Indexes
@@ -50,22 +47,17 @@ function findIndexes(nums, target){
     return idxArr
 };
 
-findIndexes([4, 2, 7, 2, 9, 2], 2)
+console.log("find indexes: ", findIndexes([4, 2, 7, 2, 9, 2], 2))
 
 
 //5. Advanced — Two Sum
 // Write a function twoSum(nums, target) that returns the indexes of two numbers that add up to target. Assume there is exactly one valid answer.
 function twoSum(nums, target){
-    const idxArr = [];
     for(let i = 0; i < nums.length - 1; i++){
         for(let j = i+1; j < nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                idxArr.push(i);
-                idxArr.push(j);
-            }
+            if(nums[i] + nums[j] === target) return [i, j]
         }
     }
-    return(idxArr)
 };
 
-twoSum([2, 7, 11, 15], 9)
+console.log("two sum: ", twoSum([2, 7, 11, 15], 9))
