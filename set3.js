60318ca064239b81472c30d66ae301e02a97e934
Lucas Carlos <lucas2carlos@gmail.com>
2026-05-20 17:23:54 -0400

    adding first non repeating

--- set3.js
+++ set3.js
@@ -3,6 +3,7 @@
 
 // 1. Beginner — Count Vowels
 // Write a function countVowels(str) that returns the number of vowels in a string.
+
 const countVowels = (str) => {
     const vowels = ["a", "e", "i", "o", "u"]
     let count = 0
@@ -17,6 +18,7 @@ console.log("Count vowels: ", countVowels("javascript"))
 
 // 2. Beginner — Reverse String
 // Write a function reverseString(str) that returns the string reversed.
+
 function reverseString(str){
     return str.split('').reverse().join('')
 }
@@ -36,3 +38,21 @@ const charFrequency = (str) => {
 }
 
 console.log("Character frequency: ", charFrequency("banana"))
+
+
+// 4. Intermediate — First Non-Repeating Character
+// Write a function firstNonRepeating(str) that returns the first character that appears only once.
+
+const firstNonRepeating = (str) => {
+    const charObj = {}
+    for (let char of str){
+        charObj[char] = (charObj[char] || 0) + 1
+    }
+
+    for(let key in charObj){
+        if(charObj[key] === 1) return key
+    }
+    return null
+}
+
+console.log("First non-repeating: ", firstNonRepeating("abbacdde"))
